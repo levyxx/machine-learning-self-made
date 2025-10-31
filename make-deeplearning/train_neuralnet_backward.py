9062a91136ec7aeb95369747c183db4e44454f88
levyxx <rb.06p.8046@s.thers.ac.jp>
2025-10-31 20:03:38 +0900

    optimize NN

--- make-deeplearning/train_neuralnet_backward.py
+++ make-deeplearning/train_neuralnet_backward.py
@@ -3,12 +3,13 @@ sys.path.append(os.pardir)
 import numpy as np
 from dataset.mnist import load_mnist
 from two_layer_net_backward import TwoLayerNetBackward
+from optimizer import AdaGrad
 
 
 if __name__ == '__main__':
     (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=False)
 
-    network = TwoLayerNetBackward(input_size=784, hidden_size=50, output_size=10)
+    network = TwoLayerNetBackward(input_size=784, hidden_size=100, output_size=10)
 
     iters_num = 10000
     train_size = x_train.shape[0]
@@ -20,6 +21,8 @@ if __name__ == '__main__':
 
     iter_per_epoch = max(train_size / batch_size, 1)
 
+    optimizer = AdaGrad(lr=learning_rate)
+
     for i in range(iters_num):
         batch_mask = np.random.choice(train_size, batch_size)
         x_batch = x_train[batch_mask]
@@ -27,8 +30,7 @@ if __name__ == '__main__':
 
         grad = network.gradient(x_batch, t_batch)
 
-        for key in ('W1', 'b1', 'W2', 'b2'):
-            network.params[key] -= learning_rate * grad[key]
+        optimizer.update(network.params, grad)
 
         loss = network.loss(x_batch, t_batch)
         train_loss_list.append(loss)
--- make-deeplearning/two_layer_net_backward.py
+++ make-deeplearning/two_layer_net_backward.py
@@ -10,15 +10,27 @@ from common.functions import *
 class TwoLayerNetBackward:
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
         self.params = {}
-        self.params['W1']=weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['W1']=np.random.randn(input_size, hidden_size) / np.sqrt(input_size) * np.sqrt(2)
         self.params['b1']=np.zeros(hidden_size)
-        self.params['W2']=weight_init_std * np.random.randn(hidden_size, output_size)
-        self.params['b2']=np.zeros(output_size)
+        self.params['W2']=np.random.randn(hidden_size, hidden_size) / np.sqrt(hidden_size) * np.sqrt(2)
+        self.params['b2']=np.zeros(hidden_size)
+        self.params['W3']=np.random.randn(hidden_size, hidden_size) / np.sqrt(hidden_size) * np.sqrt(2)
+        self.params['b3']=np.zeros(hidden_size)
+        self.params['W4']=np.random.randn(hidden_size, hidden_size) / np.sqrt(hidden_size) * np.sqrt(2)
+        self.params['b4']=np.zeros(hidden_size)
+        self.params['W5']=np.random.randn(hidden_size, output_size) / np.sqrt(hidden_size) * np.sqrt(2)
+        self.params['b5']=np.zeros(output_size)
 
         self.layers = OrderedDict()
         self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
         self.layers['Relu1'] = Relu()
         self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+        self.layers['Relu2'] = Relu()
+        self.layers['Affine3'] = Affine(self.params['W3'], self.params['b3'])
+        self.layers['Relu3'] = Relu()
+        self.layers['Affine4'] = Affine(self.params['W4'], self.params['b4'])
+        self.layers['Relu4'] = Relu()
+        self.layers['Affine5'] = Affine(self.params['W5'], self.params['b5'])
         
         self.lastLayer = SoftmaxWithLoss()
 
@@ -47,6 +59,12 @@ class TwoLayerNetBackward:
         grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
         grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
         grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+        grads['W3'] = numerical_gradient(loss_W, self.params['W3'])
+        grads['b3'] = numerical_gradient(loss_W, self.params['b3'])
+        grads['W4'] = numerical_gradient(loss_W, self.params['W4'])
+        grads['b4'] = numerical_gradient(loss_W, self.params['b4'])
+        grads['W5'] = numerical_gradient(loss_W, self.params['W5'])
+        grads['b5'] = numerical_gradient(loss_W, self.params['b5'])
 
         return grads
     
@@ -66,5 +84,11 @@ class TwoLayerNetBackward:
         grads['b1'] = self.layers['Affine1'].db
         grads['W2'] = self.layers['Affine2'].dW
         grads['b2'] = self.layers['Affine2'].db
+        grads['W3'] = self.layers['Affine3'].dW
+        grads['b3'] = self.layers['Affine3'].db
+        grads['W4'] = self.layers['Affine4'].dW
+        grads['b4'] = self.layers['Affine4'].db
+        grads['W5'] = self.layers['Affine5'].dW
+        grads['b5'] = self.layers['Affine5'].db
 
         return grads
